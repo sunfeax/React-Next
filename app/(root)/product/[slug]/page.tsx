223b247fc5c4ed21a450af31179228b4b5981bb6
Vlad Uski <sunfeax@gmail.com>
2025-12-09 23:07:37 +0100

    enhance product detail page with improved layout and error handling; update Prisma configuration and add sample environment file

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,29 +1,49 @@
+import NotFound from "@/app/not-found";
+import ProductPrice from "@/components/shared/product/product-price";
 import { Button } from "@/components/ui/button";
 import { getProductBySlug } from "@/lib/actions/product.actions";
+import { Star } from "lucide-react";
+import Image from "next/image";
 
 export default async function ProductDetailPage({params}:{params:Promise<{slug:string}>}) {
+  
   const { slug } = await params;
   const product = await getProductBySlug(slug);
+  
+  if (!product) {
+    return NotFound();
+  }
 
   return (
-    <div className="flex gap-6">
+    <div className="flex gap-20 items-start">
       <div>
-        <img className="w-xs" src={product?.images[0]} alt={product?.name} />
+        <Image
+          className="w-full max-w-xs rounded-2xl"
+          src={product.images[0]}
+          alt={product.name}
+          width={300} height={300}
+        />
       </div>
       <div>
         <p className="text-sm mb-3">{product?.category}</p>
         <h1 className="text-xl font-bold mb-5">{product?.name}</h1>
-        <p className="text-sm">{product?.rating} of {product?.numReviews} reviews</p>
+        <div className="flex text-sm items-center gap-1 mb-8">
+          <Star className="h-4 w-4 fill-amber-500 stroke-0" />
+          {product?.rating} of {product?.numReviews} reviews
+        </div>
         <div>
           <h1 className="text-l font-bold">Description</h1>
           <p>{product?.description}</p>
         </div>
       </div>
-      <div>
-        <p>Price €{product?.price}</p>
-        <div className="flex gap-2">
-          <h3>Status</h3>
-          <p>{product!.stock > 0 ? 'In stock' : 'Out of stock'}</p>
+      <div className="border-1 shadow-2xl p-5 rounded-2xl">
+        <div className="flex gap-3 mb-4">
+          Price
+          {product.stock > 0 ? (
+            <ProductPrice value={+product.price} className={""} />
+          ) : (
+            <p className="text-destructive">Out of stock</p>
+          )}
         </div>
         <Button>Add to cart</Button>
       </div>
